d722406ef27e41dff37c5632fb497dedded0c047
Orestes Febles <ofebles@gmail.com>
2022-03-09 22:14:22 -0500

    Initil commit2

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-const withPWA = require('next-pwa')
+const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    dest: "public",
     fallbacks: {
-      image: '/static/images/fallback.png',
+      image: "/static/images/fallback.png",
       // document: '/other-offline',  // if you want to fallback to a custom page other than /_offline
       // font: '/static/font/fallback.woff2',
-      // audio: ...,
-      // video: ...,
-    }
-  }
-})
+      // audio: ....,
+      // video: ....,
+    },
+  },
+});
